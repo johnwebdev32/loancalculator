a81d773eae8c64b8e37b662d78bf85fbc6cc6d0a
johnwebdev32 <105568615+johnwebdev32@users.noreply.github.com>
2022-06-03 08:44:18 -0700

    began JavaScript

--- js/site.js
+++ js/site.js
@@ -1,4 +1,35 @@
-//Call Hello World
-function helloWorld(){
-    alert("hello World");
+//Get values from user
+    
+function getValues(){
+    let loanAmount = document.getElementById("loanAmount").value;
+    let termMonths = document.getElementById("termMonths").value;
+    let interestRate = document.getElementById("interestRate").value;
+
+    loanAmount = parseInt(loanAmount);
+    termMonths = parseInt(termMonths);
+    interestRate = parseInt(interestRate);
+
+    if (Number.isInteger(loanAmount) && Number.isInteger(termMonths) && Number.isInteger(interestRate)){
+
+        let paymentArray = loanCalc(loanAmount, termMonths, interestRate);
+
+        displayData(paymentArray);
+    }
+    else{
+        alert("You must enter numbers");
+    }
+
 }
+
+function loanCalc(){
+
+
+
+
+    
+}
+//Make calculations
+
+//Display results to user
+
+
